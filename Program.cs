f5d5da2d2e3529a7383297388c26d49cccfe3c81
madu <maduamaral2006@gmail.com>
2023-06-29 13:58:21 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿double milhas;
+double km;
+
+    Console.WriteLine("");
+    Console.WriteLine("este é um convertor de");
+    Console.WriteLine("MILHAS para QUILÔMETROS");
+    Console.WriteLine("");
+    Console.WriteLine("digite o valor em milhas que deseja converter");
+
+milhas = Convert.ToDouble(Console.ReadLine());
+km = milhas * 1.609;
+
+    Console.WriteLine(""); 
+    Console.WriteLine($"{milhas} são equivalentes a {km} kms");
+    Console.WriteLine("");
+    Console.WriteLine("obrigado por utilizar este convertor :)");
+    Console.WriteLine("");
